f5f0b4f580f2b7b53d7cc5022edfab3666e20c58
opilane <opilane@HP-CZC6297B8C>
2021-02-19 15:49:16 +0200

    Add project files.

---
+++ t/t/Views/Item1.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace t.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Item1 : ContentPage
+    {
+        public Item1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btn_Clicked(object sender, EventArgs e)
+        {
+            Uri uri = new Uri("https://jojo.fandom.com/ru/wiki/Джорно_Джованна");
+            await Browser.OpenAsync(uri);
+        }
+    }
+}
